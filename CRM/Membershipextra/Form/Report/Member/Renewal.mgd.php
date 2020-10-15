73d69d42b8db8ab6481638a31864c6d7526843cb
Sunil Pawar <sunil@skvare.com>
2020-10-15 20:23:30 +0530

    Added Report for Membership Renewal

---
+++ CRM/Membershipextra/Form/Report/Member/Renewal.mgd.php
@@ -0,0 +1,15 @@
+<?php
+return [
+  0 => [
+    'name' => 'Membership Renewal Report',
+    'entity' => 'ReportTemplate',
+    'params' => [
+      'version' => 3,
+      'label' => 'Membership Renewal Report',
+      'description' => 'Membership Renewal Report',
+      'class_name' => 'CRM_Membershipextra_Form_Report_Member_Renewal',
+      'report_url' => 'member/membershiprenewal',
+      'component' => 'CiviMember',
+    ],
+  ],
+];
